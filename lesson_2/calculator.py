6f44aca35795893b5527f91761f7bb9958346b84
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-06-17 15:03:10 -0600

    calculator.py completed.

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,26 +1,46 @@
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f"==> {message}")
 
-print("What's the first number?") 
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
 number1 = input()   # Ask the user for the first number.
 
-print("What's the second number?")
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
 number2 = input()   # Ask the user for the second number.
 
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
-operator = input()  # Ask the user for an operation to perform.
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
 
-if operator == '1':
-    output = int(number1) + int(number2)    # Perform the operation on the two numbers.
+prompt("""What operation would you like to perform
+1) Add 2) Subtract 3) Multiply 4) Divide""")
+operator = input()
 
-elif operator == '2':
-    output = int(number1) - int(number2)    # Perform the operation on the two numbers.
+while operator not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operator = input()
 
-elif operator == '3':
-    output = int(number1) * int(number2)    # Perform the operation on the two numbers.
+match operator:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output= int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
 
-elif operator == '4':
-    output = int(number1) / int(number2)    # Perform the operation on the two numbers.
-
-print(f"The result is: {output}")   # Print the result to the terminal.
-
-
+prompt(f"The result is: {output}")   # Print the result to the terminal.
